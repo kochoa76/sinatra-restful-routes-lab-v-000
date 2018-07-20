87de2fb7b33e8046bba2aa2679661ef8c660f9fa
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-07-20 18:20:26 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,9 +24,9 @@ class ApplicationController < Sinatra::Base
     erb :show
   end
 
-  get '/recipes/:id/edit' do 
+  get '/recipes/:id/edit' do
     @recipe = Recipe.find_by_id(params[:id])
-    erb :edit 
+    erb :edit
   end 
 
   delete '/recipes/:id/delete' do
